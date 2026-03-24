ec5156f2784db3f6178efd9e39309039a22151fc
국동균 <kookjd7759@naver.com>
2026-03-24 10:40:04 +0900

    update

---
+++ 13. AUTOSAR/CAN_Communication/Configuration/System/DBImport/Project_version.h
@@ -0,0 +1,11 @@
+/*
+* Date: 2026. 03. 24.
+* Path: C:/Users/Donggyun/Desktop/설치 파일/Projects/edu_mpc5606b_r2401_hot_can_candb/Project.dbc
+* * [Invalid Version Number] Version Number should be number.* Project_VERSION : Empty*/
+#define Project_VERSION 
+#define Project_VERSIONNUMBERINTERNAL 0
+#define Project_VERSIONUMBER 0
+#define Project_VERSIONWEEK 1
+#define Project_VERSIONYEAR 0
+#define Project_VERSIONMONTH 1
+#define Project_VERSIONDAY 1
--- 13. AUTOSAR/README.md
+++ 13. AUTOSAR/README.md
@@ -3,3 +3,5 @@
  - RTE - Timing Event, Sender-Receiver, Client-Server를 하나의 흐름으로 구성해 스위치 입력에 따라 LED를 제어하는 애플리케이션을 설계·구현한 통합 실습  
 
  - Hardware_Abstraction - LED 점등·점멸 제어와 가변저항 입력 기반 밝기 조절을 통해 하드웨어 입출력 추상화 구조를 구현한 실습  
+
+ - CAN_Communication - CANDB Import와 BSW 모듈 설정, SWC-BSW 간 Sender-Receiver 통신, Data Received Event 기반 CAN 데이터 수신을 통해 외부 CAN 메시지를 애플리케이션에 연동한 실습
